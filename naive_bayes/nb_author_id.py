affea9208a39ba246073be5ea3c02a7bb4447ca0
Vivek Nagbansi <nagbansi@gmail.com>
2018-06-13 09:32:39 -0700

    Create and train a Naive Bayes classifier

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -19,15 +19,17 @@ from email_preprocess import preprocess
 ### features_train and features_test are the features for the training
 ### and testing datasets, respectively
 ### labels_train and labels_test are the corresponding item labels
+t0 = time()
 features_train, features_test, labels_train, labels_test = preprocess()
-
 
 
 
 #########################################################
 ### your code goes here ###
-
-
+from sklearn.naive_bayes import GaussianNB
+clf = GaussianNB()
+clf.fit(features_train, labels_train) 
+pred = clf.predict(features_test)
 #########################################################
 
 
