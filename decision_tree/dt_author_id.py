e2eb5720e29674daef9c0ea22f83ff68e57b6091
Vivek Nagbansi <nagbansi@gmail.com>
2018-06-17 09:44:49 -0700

    Email DecisionTree: Accuracy

--- decision_tree/dt_author_id.py
+++ decision_tree/dt_author_id.py
@@ -12,7 +12,8 @@ import sys
 from time import time
 sys.path.append("../tools/")
 from email_preprocess import preprocess
-
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score
 
 ### features_train and features_test are the features for the training
 ### and testing datasets, respectively
@@ -24,7 +25,11 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
-
+clf = DecisionTreeClassifier(min_samples_split=40)
+clf.fit(features_train, labels_train)
+pred = clf.predict(features_test)
+print accuracy_score(pred, labels_test)
+print len(features_train[0])
 
 #########################################################
 
