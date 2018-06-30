84a7e3d79193cdba121a08381c44a871b22ecf3a
Vivek Nagbansi <nagbansi@gmail.com>
2018-06-29 22:18:34 -0700

    Sneak Peek: Outliers Break Regressions

--- regression/finance_regression.py
+++ regression/finance_regression.py
@@ -22,8 +22,8 @@ dictionary = pickle.load( open("../final_project/final_project_dataset_modified.
 
 ### list the features you want to look at--first item in the 
 ### list will be the "target" feature
-#features_list = ["bonus", "salary"]
-features_list = ["bonus", "long_term_incentive"]
+features_list = ["bonus", "salary"]
+#features_list = ["bonus", "long_term_incentive"]
 data = featureFormat( dictionary, features_list, remove_any_zeroes=True)
 target, features = targetFeatureSplit( data )
 
@@ -96,6 +96,18 @@ try:
     plt.plot( feature_test, reg.predict(feature_test) )
 except NameError:
     pass
+
+
+#Now we'll be drawing two regression lines, one fit on the test data (with outlier) and one fit on the training data (no outlier).
+reg.fit(feature_test, target_test)
+plt.plot(feature_train, reg.predict(feature_train), color="b")
+
+#What's the slope of the new regression line?
+print "\nSlope/Intercept of the new regression line"
+print "Coef: ", reg.coef_
+print "Intercept: ", reg.intercept_
+
+
 plt.xlabel(features_list[1])
 plt.ylabel(features_list[0])
 plt.legend()
